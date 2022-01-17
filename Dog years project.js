460ebe760ce47a9cd749d0f2f2b5c84ada4964da
Godwin-Anagbo <93626600+Godwin-Anagbo@users.noreply.github.com>
2022-01-17 02:00:39 +0000

    Create Dog years project.js

---
+++ Dog years project.js
@@ -0,0 +1,16 @@
+// this line sets my age as a number
+let myAge = 80;
+// this line sets 2 years to earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+//this line computes the value of later years
+let laterYears = myAge - 2;
+// this line multiplies laterYears by 4 and updates the value
+laterYears *= 4;
+//myAgeInDogYears is an addition of earlyYears and laterYears
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Godwin';
+//this line returens the name with all lowercase letters
+myName = myName.toLowerCase(myName);
+//this line prints name, human years and dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in Human years which is ${myAgeInDogYears} years old in dog years.`)
